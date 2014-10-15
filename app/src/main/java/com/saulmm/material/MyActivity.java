5523fcba4939f36efa31a392708f5890e4dade8e
Saul M <saulmm2@gmail.com>
2014-10-16 00:59:19 +0200

    Added a transition between activities with shared elements

--- app/src/main/java/com/saulmm/material/MyActivity.java
+++ app/src/main/java/com/saulmm/material/MyActivity.java
@@ -1,16 +1,28 @@
 package com.saulmm.material;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.graphics.Outline;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Scene;
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.saulmm.material.R;
 
 
 public class MyActivity extends Activity {
+
+    private FrameLayout frameContainer;
+    private View holderView;
+    private View fabButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,19 +35,27 @@ public class MyActivity extends Activity {
         Outline fabOutLine = new Outline();
         fabOutLine.setOval(0, 0, fabSize, fabSize);
 
+        // Fragment container
+         frameContainer = (FrameLayout) findViewById(R.id.container);
+
+        holderView = findViewById(R.id.holder_view);
+
         // FabView
-        View fabView = findViewById(R.id.fab_button);
-        fabView.setOnClickListener(fabClickListener);
-        fabView.setOutline(fabOutLine);
+        fabButton = findViewById(R.id.fab_button);
+        fabButton.setOnClickListener(fabClickListener);
+        fabButton.setOutline(fabOutLine);
     }
 
 
     View.OnClickListener fabClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MyActivity.this,
+                    Pair.create(holderView, "holder2"),
+                    Pair.create(fabButton, "fab"));
 
-            Toast.makeText(MyActivity.this, "Hi",
-                Toast.LENGTH_SHORT).show();
+            Intent i  = new Intent (MyActivity.this, MyActivity2.class);
+            startActivity(i, options.toBundle());
         }
     };
 }
---
+++ app/src/main/java/com/saulmm/material/MyActivity2.java
@@ -0,0 +1,43 @@
+package com.saulmm.material;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.graphics.Outline;
+import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Explode;
+import android.transition.Scene;
+import android.transition.Slide;
+import android.transition.TransitionManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.saulmm.material.R;
+
+public class MyActivity2 extends Activity {
+
+    private FrameLayout frameContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my2);
+
+        // Fab Button
+        int fabSize = getResources().getDimensionPixelSize(R.dimen.fab_size);
+        Outline fabOutLine = new Outline();
+        fabOutLine.setOval(0, 0, fabSize, fabSize);
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        frameContainer = (FrameLayout) findViewById(R.id.container);
+
+        Scene scene1 = Scene.getSceneForLayout(frameContainer, R.layout.activity_my_activity2, this);
+        TransitionManager.go(scene1, new Explode());
+    }
+}
