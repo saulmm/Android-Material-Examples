277be8d4e28481451eaf693b3612e952f4490848
Saul M <saulmm2@gmail.com>
2014-10-17 03:06:43 +0200

    some refactorizations & added Readme

---
+++ README.md
@@ -0,0 +1,11 @@
+### Working with shared elements
+
+Commit - **8872619**
+
+![](https://lh4.googleusercontent.com/-dmDFoF7c5UY/VD77NsFK8kI/AAAAAAAAuLM/MtSqP8JoQco/w282-h499-no/2014-10-16%2B00_51_34.gif)
+
+### Working with shared elements, explode transition, and animations
+
+Commit - **901d62c**
+
+![](https://lh6.googleusercontent.com/-ecoWQejLmFE/VEBoRo3Qs_I/AAAAAAAAuTU/YZ7855GmOfw/w276-h498-no/appear.gif)
--- app/src/main/java/com/saulmm/material/MyActivity2.java
+++ app/src/main/java/com/saulmm/material/MyActivity2.java
@@ -1,32 +1,17 @@
 package com.saulmm.material;
 
 import android.animation.Animator;
-import android.animation.LayoutTransition;
 import android.app.Activity;
-import android.app.Fragment;
-import android.graphics.Outline;
 import android.os.Bundle;
-import android.transition.ChangeBounds;
-import android.transition.CircularPropagation;
-import android.transition.Explode;
-import android.transition.Scene;
-import android.transition.Slide;
 import android.transition.Transition;
-import android.transition.TransitionManager;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewPropertyAnimator;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
-
-import com.saulmm.material.R;
 
 public class MyActivity2 extends Activity {
 
     private static final int NUM_VIEWS = 5;
-    private static final int SCALE_ITEM_ANIMATION_DELAY = 30;
+    private static final int SCALE_DELAY = 30;
     private LinearLayout rowContainer;
 
     @Override
@@ -59,7 +44,7 @@ public class MyActivity2 extends Activity {
                 for (int i = 0; i < rowContainer.getChildCount(); i++) {
 
                     View rowView = rowContainer.getChildAt(i);
-                    rowView.animate().setStartDelay(i * SCALE_ITEM_ANIMATION_DELAY)
+                    rowView.animate().setStartDelay(i * SCALE_DELAY)
                             .scaleX(1).scaleY(1);
                 }
             }
@@ -72,7 +57,7 @@ public class MyActivity2 extends Activity {
         for (int i = 0; i < rowContainer.getChildCount(); i++) {
 
             View rowView = rowContainer.getChildAt(i);
-            ViewPropertyAnimator propertyAnimator = rowView.animate().setStartDelay(i * SCALE_ITEM_ANIMATION_DELAY)
+            ViewPropertyAnimator propertyAnimator = rowView.animate().setStartDelay(i * SCALE_DELAY)
                 .scaleX(0).scaleY(0);
 
             propertyAnimator.setListener(new Animator.AnimatorListener() {
