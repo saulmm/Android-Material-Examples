265afae8d5bf17a1fc20c92590517fd7f0e0ae66
Saul M <saulmm2@gmail.com>
2015-08-23 09:45:52 +0200

    Changed layout and behaviour from ColorActivity

---
+++ app/src/main/java/com/saulmm/material/activities/ColorActivity2.java
@@ -0,0 +1,14 @@
+package com.saulmm.material.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.saulmm.material.R;
+
+public class ColorActivity2 extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_color2);
+	}
+}
--- app/src/main/java/com/saulmm/material/activities/MainActivity.java
+++ app/src/main/java/com/saulmm/material/activities/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity implements
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
-
 		initUI();
 		initToolbar();
 		initNavigationView();
