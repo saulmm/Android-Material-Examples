3f6765355940f6f92a05729bafa45b86361acca3
Saul M <saulmm2@gmail.com>
2015-08-23 11:53:32 +0200

    End ColorActivity refactorizations

--- app/src/main/java/com/saulmm/material/activities/ColorActivity.java
+++ app/src/main/java/com/saulmm/material/activities/ColorActivity.java
@@ -4,27 +4,35 @@ import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import com.saulmm.material.R;
 import com.saulmm.material.utils.AnimatorAdapter;
 
 public class ColorActivity extends Activity {
 
-    public final static float SCALE_FACTOR = 20f;
-    public final static int ANIMATION_DURATION  = 300;
+    public final static float SCALE_FACTOR          = 20f;
+    public final static int ANIMATION_DURATION      = 300;
+
+    public final static int SECOND_ACTIVITY_REQUEST = 1;
+    public final static int SECOND_ACTIVITY_END     = 1;
+    private FloatingActionButton mFab;
+    private boolean mReturning;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+
+        mFab = (FloatingActionButton) findViewById(R.id.activity_color_fab);
     }
 
-    public void start(View view) {
+    public void onFabCLick(View view) {
 
-        view.setClickable(false);
+        mReturning = false;
 
-        view.animate()
+        mFab.animate()
             .scaleXBy(SCALE_FACTOR)
             .scaleYBy(SCALE_FACTOR)
             .setListener(fabAnimAdapter)
@@ -34,10 +42,32 @@ public class ColorActivity extends Activity {
 
     AnimatorAdapter fabAnimAdapter = new AnimatorAdapter() {
         @Override public void onAnimationEnd(Animator animation) {
-
             super.onAnimationEnd(animation);
-            startActivity(new Intent(ColorActivity.this, ColorActivity2.class));
-            overridePendingTransition(0, 0);
+
+            if (!mReturning) {
+
+                Intent secondActivityIntent = new Intent(ColorActivity.this, ColorActivity2.class);
+                secondActivityIntent .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivityForResult(secondActivityIntent, SECOND_ACTIVITY_REQUEST);
+            }
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SECOND_ACTIVITY_REQUEST && resultCode == SECOND_ACTIVITY_END) {
+
+            mReturning = true;
+
+            mFab.animate()
+                .scaleXBy(SCALE_FACTOR)
+                .scaleYBy(SCALE_FACTOR)
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(ANIMATION_DURATION)
+                .start();
+        }
+    }
 }
--- app/src/main/java/com/saulmm/material/activities/ColorActivity2.java
+++ app/src/main/java/com/saulmm/material/activities/ColorActivity2.java
@@ -2,13 +2,69 @@ package com.saulmm.material.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 import com.saulmm.material.R;
 
 public class ColorActivity2 extends AppCompatActivity {
+
+	private TextView mHelpText;
+	private Toolbar mToolbar;
+	private View mContainer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_color2);
+
+		mContainer = findViewById(R.id.activity_color2_container);
+		mToolbar = (Toolbar) findViewById(R.id.activity_color2_toolbar);
+		mHelpText = (TextView) findViewById(R.id.activity_color2_explanation_textview);
+		mHelpText.setVisibility(View.VISIBLE);
+
+		Animation translateToolbarAnimation = AnimationUtils.loadAnimation(this, R.anim.translate_up_on);
+		mToolbar.setAnimation(translateToolbarAnimation);
+
+		Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.abc_fade_in);
+		fadeIn.setStartOffset(300);
+		fadeIn.setDuration(800);
+		mHelpText.setAnimation(fadeIn);
+
+		AnimationSet animationSet = new AnimationSet(true);
+		animationSet.addAnimation(translateToolbarAnimation);
+		animationSet.addAnimation(fadeIn);
+		animationSet.start();
+	}
+
+	@Override
+	public void onBackPressed() {
+
+		Animation fadeOut = AnimationUtils.loadAnimation(this, R.anim.abc_fade_out);
+		fadeOut.setFillAfter(true);
+		mContainer.startAnimation(fadeOut);
+
+		fadeOut.setAnimationListener(new Animation.AnimationListener() {
+			@Override public void onAnimationStart(Animation animation) {
+
+			}
+
+			@Override public void onAnimationEnd(Animation animation) {
+				setResult(ColorActivity.SECOND_ACTIVITY_END);
+				populateOnBackPressed();
+			}
+
+			@Override public void onAnimationRepeat(Animation animation) {
+
+			}
+		});
+	}
+
+	public void populateOnBackPressed() {
+		finish();
+		overridePendingTransition(0, 0);
 	}
 }
