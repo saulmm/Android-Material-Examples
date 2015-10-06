82eaec55ea8cc25f42db91234febe2656e24e48e
Saul M <saulmm2@gmail.com>
2015-10-06 19:34:55 +0200

    Multiple refactorizations, mostly on the reveal activities

--- app/src/main/java/com/saulmm/material/activities/RevealFirstActivity.java
+++ app/src/main/java/com/saulmm/material/activities/RevealFirstActivity.java
@@ -5,25 +5,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import com.saulmm.material.R;
 import com.saulmm.material.utils.AnimatorAdapter;
 
-public class ColorActivity extends Activity {
+@SuppressWarnings("FieldCanBeLocal")
+public class RevealFirstActivity extends Activity {
     public final static float SCALE_FACTOR          = 20f;
     public final static int ANIMATION_DURATION      = 300;
 
     public final static int SECOND_ACTIVITY_REQUEST = 1;
     public final static int SECOND_ACTIVITY_END     = 1;
-    private FloatingActionButton mFab;
     private boolean mReturning;
+
+    private FloatingActionButton mFab;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_color);
+        setContentView(R.layout.activity_reveal_first);
 
-        mFab = (FloatingActionButton) findViewById(R.id.activity_color_fab);
+        mFab = (FloatingActionButton) findViewById(R.id.activity_reveal_fab);
+        mToolbar = (Toolbar) findViewById(R.id.activity_reveal_toolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     public void onFabCLick(View view) {
@@ -42,8 +52,7 @@ public class ColorActivity extends Activity {
             super.onAnimationEnd(animation);
 
             if (!mReturning) {
-
-                Intent secondActivityIntent = new Intent(ColorActivity.this, ColorActivity2.class);
+                Intent secondActivityIntent = new Intent(RevealFirstActivity.this, RevealSecondActivity.class);
                 secondActivityIntent .addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 startActivityForResult(secondActivityIntent, SECOND_ACTIVITY_REQUEST);
             }
